36c66161dd6d591439dc021085592bcdaa9c70ca
rahul-ongraph <rahul-ongraph@gmail.com>
2021-03-29 21:31:48 +0530

    code added

--- src/component/Checkbox/Checkbox.js
+++ src/component/Checkbox/Checkbox.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./../Checkbox/Checkbox.css";
 import { Checkbox } from "antd";
 
-const CategoryBox = ({ item1,onChange }) => {
+const CategoryBox = ({ item1,onChange}) => {
   return (
     <div>
+      <Checkbox.Group style={{ width: '100%' }} onChange={onChange}>
       {item1.map((item, index) => (
-        <Checkbox className="checkbox" onChange={onChange}  level={2} value={item.value}>
+        <Checkbox className="checkbox" level={2} 
+        value={item.item}   >
           {item.item}
         </Checkbox>
       ))}
+     </Checkbox.Group>
     </div>
   );
 };
--- src/container/Product/Product.js
+++ src/container/Product/Product.js
@@ -1,7 +1,7 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../Product/Product.css";
 import CategoryBox from "../../component/Checkbox/Checkbox";
-import { Layout, AutoComplete, Input, Carousel, Image, Typography ,Button} from "antd";
+import { Layout, AutoComplete, Input, Carousel, Image, Typography, Button } from "antd";
 import Card from "../../component/Card/Card";
 import {
   HomeFilled,
@@ -18,18 +18,12 @@ const item4 = [
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/realmec11.jpeg",
       },
       {
-        id: 2,
-        value: 4,
         item: "/assets/realme1.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/realme2.jpeg",
       },
     ],
@@ -38,258 +32,245 @@ const item4 = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Electronics",
-    brand:"Realme",
+    brand: "Realme",
     rate: 5,
   },
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/jeans.jpeg",
       },
       {
-        id: 2,
-        value: 4,
         item: "/assets/jeans1.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/jeans3.jpeg",
       },
     ],
-    price:1999,
+    price: 1999,
     productName: "Jeans(Highlander)",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Clothes",
-    brand:"Highlander",
+    brand: "Highlander",
     rate: 4,
   },
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/oil.jpeg",
       },
-      { 
-        id: 2,
-        value: 4,
+      {
         item: "/assets/oil1.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/oil2.jpeg",
       },
     ],
-    price:750,
+    price: 750,
     productName: "Fortuner oil",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Grocery",
-    brand:"Fortuner",
-    rate:5,
+    brand: "Fortuner",
+    rate: 5,
   },
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/samsung1.jpeg",
       },
-      { 
-        id: 2,
-        value: 4,
+      {
         item: "/assets/samsung2.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/samsung3.jpeg",
       },
     ],
-    price:24999,
+    price: 24999,
     productName: "Samsung Refrigator(415 L)",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Electronics",
-    brand:"Samsung",
+    brand: "Samsung",
     rate: 5,
   },
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/shirt.jpeg",
       },
-      { 
-        id: 2,
-        value: 4,
+      {
         item: "/assets/shirt1.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/shirt2.jpeg",
       },
     ],
-    price:1499,
+    price: 1499,
     productName: "Shirt(Peter England)",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Clothes",
-    brand:"Peter England",
+    brand: "Peter England",
     rate: 5,
   },
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/atta1.jpeg",
       },
-      { 
-        id: 2,
-        value: 4,
+      {
         item: "/assets/atta2.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/atta3.jpeg",
       },
     ],
-    price:499 ,
+    price: 499,
     productName: "Ashirvad Atta",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Grocery",
-    brand:"Ashirvad",
+    brand: "Ashirvad",
     rate: 4.5,
   },
   {
     image: [
       {
-        id: 1,
-        value: 5,
         item: "/assets/tshirt2.jpeg",
       },
-      { 
-        id: 2,
-        value: 4,
+      {
         item: "/assets/tshirt3.jpeg",
       },
       {
-        id: 3,
-        value: 5,
         item: "/assets/tshirt1.jpeg",
       },
     ],
-    price:999,
-    productName:"tShirt",
+    price: 999,
+    productName: "tShirt",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     category: "Clothes",
-    brand:"tshirt",
-    rate:3,
+    brand: "tshirt",
+    rate: 3,
   },
 ];
 
 function Product(props) {
   const [search, setSearch] = useState("");
   const [data, setData] = useState(item4);
-  const [category,setCategory]=useState([])
+  const [category, setCategory] = useState([])
+  const [brand, setBrand] = useState([])
+  const [price, setPrice] = useState([])
   const { Header, Sider, Content } = Layout;
   const { Title } = Typography;
-  const item1 = [
+  const itemCategory = [
     {
-      id: 1,
-      value: 5,
-      item: "Grocery",
+      "item": "Grocery",
     },
     {
-      id: 2,
-      value: 4,
-      item: "Clothes",
+      "item": "Clothes",
     },
     {
-      id: 3,
-      value: 5,
-      item: "Electronics",
+
+      "item": "Electronics",
     },
   ];
- 
-  const item2 = [
+
+  const brandCategory = [
     {
-      id: 1,
-      value: 5,
       item: "Samsung",
     },
     {
-      id: 2,
-      value: 4,
       item: "Realme",
     },
     {
-      id: 3,
-      value: 5,
       item: "Highlander",
     },
     {
-      id: 4,
-      value: 3,
       item: "Fortuner",
     },
     {
-      id: 4,
-      value: 3,
       item: "Peter England",
     },
   ];
-  const item3 = [
+  const priceCategory = [
     {
-      id: 1,
-      item:"0 to 999",
+
+      item: "0 to 999",
     },
     {
-      id: 2,
-      value: 4,
+
       item: "1000 to 9999",
     },
     {
-      id: 3,
-      value: 5,
+
       item: "9999 to 29999",
     },
   ];
-  
+
   const searchFilter = (event) => {
     if (event.target.value == "") {
       setData(item4)
     } else {
-    	const temp = item4.filter((value) => (value.productName.toLowerCase().includes(event.target.value.toLowerCase())))
+      const temp = item4.filter((value) => (value.productName.toLowerCase().includes(event.target.value.toLowerCase())))
       setData(temp)
     }
     setSearch(event.target.value);
   };
 
-  function onChangeCategory (value)  {
+  function onChangeCategory(value) {
+    console.log("hhhhhhhhhhhh", value)
     if (value.length == 0) {
       setData(item4)
-      console.log("Hello",item4)
     } else {
-    	value.map((item1) => {
-        const temp = item4.filter((item) => {
-          console.log(item)
-          return item.category == item1})
-    	})
-      setData(item1)
+      let temp1 = []
+      value.map((item1) => {
+        const temp = item4.filter((item) => item.category.toLowerCase() == item1.toLowerCase())
+        console.log("hhhhhhhhhhhh", temp, value)
+        let temp2 = temp1.concat(temp)
+        temp1 = temp2
+      })
+      setData(temp1)
     }
-    setCategory(value);
+    setCategory(value)
+  }
+
+  function onChangeBrand(value) {
+    console.log("hhhhhhhhhhhh", value)
+    if (value.length == 0) {
+      setData(item4)
+    } else {
+      let temp1 = []
+      value.map((item1) => {
+        const temp = item4.filter((item) => item.brand.toLowerCase() == item1.toLowerCase())
+        console.log("hhhhhhhhhhhh", temp, value)
+        let temp2 = temp1.concat(temp)
+        temp1 = temp2
+      })
+      setData(temp1)
+    }
+    setBrand(value)
+  }
+
+  function onChangePrice(value) {
+    if (value.length == 0) {
+      setData(item4)
+    } else {
+      let temp1 = []
+      value.map((item1) => {
+        const x = item1.split(" ")
+        const min = x[0]
+        const max = x[2]
+        const temp = item4.filter((item) => item.price <= max && item.price > min)
+        console.log("aaaaaaa", temp)
+        let temp2 = temp1.concat(temp)
+        temp1 = temp2
+      })
+      setData(temp1)
+    }
+    setPrice(value)
   }
 
   return (
@@ -323,16 +304,20 @@ function Product(props) {
                 Category
               </Title>
               <CategoryBox
-              onChange={onChangeCategory}
-              item1={item1} />
+                onChange={onChangeCategory}
+                item1={itemCategory} />
               <Title className="category" style={{ color: "green" }} level={5}>
                 Brand
               </Title>
-              <CategoryBox item1={item2} />
+              <CategoryBox
+                onChange={onChangeBrand}
+                item1={brandCategory} />
               <Title className="category" style={{ color: "green" }} level={5}>
                 Price
               </Title>
-              <CategoryBox item1={item3} />
+              <CategoryBox
+                onChange={onChangePrice}
+                item1={priceCategory} />
             </div>
             <div className="productContent">
               <Carousel autoplay>
@@ -365,10 +350,9 @@ function Product(props) {
                   />
                 </div>
               </Carousel>
-              <Content  className="ContentCard">
-                <Card  item1={data} />
+              <Content className="ContentCard">
+                <Card item1={data} />
               </Content>
-              
             </div>
           </Content>
         </Layout>
